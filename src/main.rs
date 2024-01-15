0217076c65edf90c8b0b5b60a47e0a37d13c7932
w08r <will@hazy.com>
2024-01-15 12:17:10 +0000

    Clippy

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,15 @@ use async_graphql::{
     EmptyMutation,
     EmptySubscription,
     Object,
-    Schema,
-    futures_util::future::join_all
+    Schema
 };
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    routing::get,
+    response::{self, IntoResponse},
+    Router,
 };
-use std::future::IntoFuture;
-
-use tokio::task::JoinSet;
+use async_graphql_axum::GraphQL;
+use tokio::net::TcpListener;
 
 pub struct QueryRoot;
 
@@ -24,36 +22,17 @@ impl QueryRoot {
     }
 }
 
-use async_graphql_axum::GraphQL;
-use axum::{
-    response::{self, IntoResponse}
-};
-// use starwars::{QueryRoot, StarWars};
-use tokio::net::TcpListener;
-
 async fn graphiql() -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint("/").finish())
 }
-
 #[tokio::main]
 async fn main() {
-    let mut set = JoinSet::new();
-
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        // .data(StarWars::new())
         .finish();
 
     let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
 
-    println!("GraphiQL IDE: http://localhost:8000");
-
-    set.spawn(axum::serve(TcpListener::bind("0.0.0.0:8080").await.unwrap(), Router::new()).into_future());
-    set.spawn(axum::serve(TcpListener::bind("0.0.0.0:8000").await.unwrap(), app).into_future());
-
-    set.join_next().await;
-    set.join_next().await;
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
+    axum::serve(TcpListener::bind("0.0.0.0:8000").await.unwrap(), app)
+        .await
+        .unwrap();
 }
